2884a6bef6801fd079be217e950bc617f43f211b
ASBHYCEPE <ashby.cepe@unc.edu.ph>
2021-11-27 12:04:43 +0800

    Added sql select in viewing shows based on category

--- app/Http/Controllers/navigationController.php
+++ app/Http/Controllers/navigationController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class navigationController extends Controller
 {
     public function index(){
-        return view('index');
+
+        $planWatch = DB::select('SELECT * FROM watchlists WHERE show_category = 0');
+        $watching = DB::select('SELECT * FROM watchlists WHERE show_category = 1');
+        
+        return view('index',['planWatch' => $planWatch, 
+                             'watching' => $watching]);
     }
 
     public function history(){
